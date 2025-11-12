4d894bfd3f8c7824dad92cd9bfe321f67ecde2c2
Arrosyid Al Ayubi <ayubirosyid@gmail.com>
2025-11-13 00:17:35 +0700

    fix image

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,28 +2,11 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 import cloudflare from '@astrojs/cloudflare';
-import opengraphImages, { presets } from "astro-opengraph-images";
-import * as fs from "fs";
-import { customOgMediaLayout } from "./src/customRenderer";
+
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://kelas10sifp.my.id/",
-  integrations: [
-    opengraphImages({
-      options: {
-        fonts: [
-          {
-            name: "Roboto",
-            weight: 400,
-            style: "normal",
-            data: fs.readFileSync("node_modules/@fontsource/roboto/files/roboto-latin-400-normal.woff"),
-          },
-        ],
-      },
-      render: customOgMediaLayout,
-    }),
-  ],
   vite: {
     plugins: [tailwindcss()],
   },
