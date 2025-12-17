43b6df1da1a69c952f1a4be669ba761fd8488bd0
Arrosyid Al Ayubi <ayubirosyid@gmail.com>
2025-12-18 01:18:39 +0700

    Update
    delete unnecessary files and create new dosen page, and update some pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 import cloudflare from '@astrojs/cloudflare';
 import sitemap from '@astrojs/sitemap';
-import { members } from './members.js';
 
 
 // https://astro.build/config
@@ -20,12 +19,4 @@ export default defineConfig({
       enabled: true,
     },
   }),
-
-  integrations: [sitemap({
-    // This function generates sitemap entries from your members data.
-    // It assumes you will create a page at `src/pages/anggota/[subdomain].astro`
-    customPages: members.map(member => {
-      return `https://kelas10sifp.my.id/anggota/${member.subdomain}`;
-    }),
-  })],
 });
--- src/components/navLinks.js
+++ src/components/navLinks.js
@@ -2,4 +2,5 @@ export const navigationLinks = [
     { href: "/", text: "Beranda" },
     { href: "/#tentang", text: "Tentang" },
     { href: "/#anggota", text: "Anggota" },
+    { href: "/dosen", text: "Dosen" }
 ];
