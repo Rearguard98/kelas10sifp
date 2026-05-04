8f609b6191a15859b60c1f501f6c35069e384a1a
Arrosyid Al Ayubi <ayubirosyid@gmail.com>
2026-05-05 03:20:23 +0700

    update semester 2

--- src/components/navLinks.js
+++ src/components/navLinks.js
@@ -2,5 +2,6 @@ export const navigationLinks = [
     { href: "/", text: "Beranda" },
     { href: "/#tentang", text: "Tentang" },
     { href: "/#anggota", text: "Anggota" },
-    { href: "/dosen", text: "Dosen" }
+    { href: "/semester1", text: "Semester 1" },
+    { href: "/semester2", text: "Semester 2" }
 ];
