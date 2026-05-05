ef3d72776817ce3c57f08ad4e373a53de49253ba
Arrosyid Al Ayubi <ayubirosyid@gmail.com>
2026-05-05 14:27:44 +0700

    delete cloudflare astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,5 @@
 import { defineConfig } from "astro/config";
-import cloudflare from "@astrojs/cloudflare";
+
 
 export default defineConfig({
-  adapter: cloudflare({
-    platformProxy: {
-      enabled: true,
-    },
-  }),
 });
