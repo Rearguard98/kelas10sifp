f0bbca5fd70045229533a1ba2cb23df1863a8fac
Arrosyid Al Ayubi <ayubirosyid@gmail.com>
2026-05-05 13:13:00 +0700

    fix move pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,8 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
-import tailwindcss from "@tailwindcss/vite";
-import cloudflare from '@astrojs/cloudflare';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import cloudflare from "@astrojs/cloudflare";
 
-
-// https://astro.build/config
 export default defineConfig({
-  site: "https://kelas10sifp.my.id/",
-
-  vite: {
-    plugins: [tailwindcss()]
-  },
-
   adapter: cloudflare({
-    imageService: 'passthrough',
     platformProxy: {
       enabled: true,
     },
