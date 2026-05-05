886b01d0ee2088746dedec51309034367f02b094
Arrosyid Al Ayubi <ayubirosyid@gmail.com>
2026-05-05 15:08:56 +0700

    fixing semester 2 website

--- src/components/navLinks.js
+++ src/components/navLinks.js
@@ -2,6 +2,11 @@ export const navigationLinks = [
     { href: "/", text: "Beranda" },
     { href: "/#tentang", text: "Tentang" },
     { href: "/#anggota", text: "Anggota" },
-    { href: "/semester1", text: "Semester 1" },
-    { href: "/semester2", text: "Semester 2" }
+    {
+        text: "Semester",
+        submenu: [
+            { href: "/semester1", text: "Semester 1" },
+            { href: "/semester2", text: "Semester 2" }
+        ]
+    }
 ];
